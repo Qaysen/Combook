15dbe38c76f7d51109308b3b37f53e8a4064e90e
PedroVr <castillogallardopedro@hotmail.com>
2013-06-18 16:58:42 -0500

    ni quiere subir las imagenes

--- COMBOOK/settings.py
+++ COMBOOK/settings.py
@@ -53,12 +53,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = 'RUTA_PROYECTO.child("media")'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -69,12 +69,12 @@ STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/estaticos/'
-STATIC_URL = '/media/'
+
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     RUTA_PROYECTO.child('estaticos'),
-     RUTA_PROYECTO.child('media'),
+ 
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
